c48ac2c0fdfd36ebe4fd8cc1b9f02f71ef1cf005
krusty <krusty@vxn.dev>
2026-03-20 14:19:36 +0100

    Add new locale_types SQL migration file, add minor fixes

--- sql/migrations/000003_type_mission_tow.sql
+++ sql/migrations/000003_type_mission_tow.sql
@@ -2,4 +2,5 @@
 INSERT INTO "high_scores_types" VALUES (6,'TYPE_MISSION_TOW');
 
 -- +goose Down
+DELETE FROM "high_scores" WHERE type = 6;
 DELETE FROM "high_scores_types" WHERE id = 6;
---
+++ sql/migrations/000004_locale_types.sql
@@ -0,0 +1,39 @@
+-- +goose up
+CREATE TABLE IF NOT EXISTS "locale_types" (
+	"id"	INTEGER,
+	"name"	TEXT UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+INSERT INTO "locale_types" VALUES (0,'LOCALE_EN');
+INSERT INTO "locale_types" VALUES (1,'LOCALE_CZ');
+
+ALTER TABLE "users" ADD COLUMN "locale" INTEGER DEFAULT 0;
+CREATE TABLE "users_copy" AS SELECT * FROM "users";
+DROP TABLE "users";
+
+CREATE TABLE IF NOT EXISTS "users" (
+	"id"	INTEGER,
+	"nickname"	TEXT NOT NULL,
+	"pwdhash"	TEXT NOT NULL,
+	"salt"	TEXT NOT NULL,
+	"cash"	INTEGER NOT NULL,
+	"bank"	INTEGER NOT NULL,
+	"adminlvl"	INTEGER NOT NULL DEFAULT 0,
+	"wanted"	INTEGER NOT NULL DEFAULT 0,
+	"team"	INTEGER NOT NULL,
+	"class"	INTEGER NOT NULL,
+	"health"	INTEGER NOT NULL,
+	"armour"	INTEGER NOT NULL,
+	"spawn"	INTEGER NOT NULL,
+	"properties"	TEXT,
+	"locale" INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("locale") REFERENCES "locale_types"("id")
+);
+
+INSERT INTO "users" SELECT * FROM "users_copy";
+DROP TABLE "users_copy";
+
+-- +goose Down
+ALTER TABLE "users" DROP COLUMN "locale";
+DROP TABLE IF EXISTS "locale_types";
